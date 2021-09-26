d5fa303ee86f268fd01f6ae28a26f5aa3ee4e7a4
Ivo Fernandes <ivomuze@gmail.com>
2021-09-26 01:43:00 +0100

    Bottom bar navigation with animation

--- turing_deal/lib/ticker/tickerScreen.dart
+++ turing_deal/lib/ticker/tickerScreen.dart
@@ -9,10 +9,18 @@ import 'package:turing_deal/ticker/chart/turingDealChart.dart';
 
 import 'details/listPricesTextUI.dart';
 
-class TickerScreen extends StatelessWidget {
+class TickerScreen extends StatefulWidget {
   StockTicker ticker;
 
   TickerScreen(this.ticker);
+
+  @override
+  State<TickerScreen> createState() => _TickerScreenState();
+}
+
+class _TickerScreenState extends State<TickerScreen> {
+  int _selectedIndex = 0;
+  PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,23 +29,54 @@ class TickerScreen extends StatelessWidget {
       Provider.of<BigPictureStateProvider>(context, listen: false);
 
     return FutureBuilder<dynamic>(
-        future: bigPictureState.getTickerData(this.ticker),
+        future: bigPictureState.getTickerData(this.widget.ticker),
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
           return SafeArea(
             child: Scaffold(
                 appBar: AppBar(
-                  title: Text(TickerResolve.getTickerDescription(this.ticker))
+                  title: Text(TickerResolve.getTickerDescription(this.widget.ticker))
                 ),
                 body: snapshot.data == null
                     ? Center(child: CircularProgressIndicator())
                     : Container(
                       padding: const EdgeInsets.all(20.0),
-                      child: TuringDealChart(snapshot.data as List<CandlePrice>),
-                   //ListPricesText((snapshot.data as List<CandlePrice>).reversed.toList())
-                    )
+                      child: PageView(
+                        controller: _pageController,
+                        onPageChanged: _onItemSelected,
+                        children: [
+                          TuringDealChart(snapshot.data as List<CandlePrice>),
+                          ListPricesText((snapshot.data as List<CandlePrice>).reversed.toList())
+                        ],
+                      )
+                    ),
+              bottomNavigationBar: BottomNavigationBar(
+                onTap: _onItemSelected,
+                currentIndex: _selectedIndex,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.show_chart_outlined),
+                    label: 'Chart'
+                  ),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.list),
+                      label: 'Prices'
+                  ),
+                ],
+              ),
         ),
           );
       }
     );
+  }
+
+  void _onItemSelected(int index){
+    setState(() {
+      _pageController.animateToPage(
+          index,
+          duration: Duration(milliseconds: 200),
+          curve: Curves.easeOut);
+      _selectedIndex = index;
+    });
+    print(index.toString());
   }
 }
